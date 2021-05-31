c512a0eb322f2669e862d15b4ee030a5f54d85d6
RedMadda <maddalenarossaro91@gmail.com>
2021-05-31 17:36:57 +0200

    no includes

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,10 +3,17 @@ var emailsClub = ["mai@gmail.com", "na@gmail.com", "gioia@gmail.com", "fataTurch
 
 var mailUtente = prompt("Inserisci il tuo indirizzo e-mail, per favore!");
 
-var check = emailsClub.includes(mailUtente);
-
-if (check){
-    alert("Sei iscritto al nostro Club!")
-} else {
-    alert("Non puoi accedere, non sei iscritto!")
+// var check = emailsClub.includes(mailUtente);
+for (i=0; i < emailsClub.length; i++){
+    if(emailsClub[i] === mailUtente){
+        document.getElementById("stampa").innerHTML = "Sei fra i nostri iscritti!"
+    } else {
+        document.getElementById("stampa").innerHTML = "Non sei un nostro membro, per ora!"
+    }
 }
+
+// if (check){
+//     alert("Sei iscritto al nostro Club!")
+// } else {
+//     alert("Non puoi accedere, non sei iscritto!")
+// }
