c2bea2f3105ac186ac2d38d662160ffd87c0d2cc
RedMadda <maddalenarossaro91@gmail.com>
2021-05-31 15:13:20 +0200

    init

---
+++ lista-cognomi/js/script.js
@@ -0,0 +1,7 @@
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trovachiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
